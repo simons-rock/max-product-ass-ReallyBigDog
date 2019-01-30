25b08fb02a040de23b31658b437ac38a677bec2e
Eli Garcia <eligarcia@Elis-MacBook-Pro.local>
2019-01-29 22:48:10 -0500

    big dog is done

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,6 +1,43 @@
 #include "max_product.h"
 
 int max_pairwise_product_fast(int *arr, int size){
-	return 0;
+	/*int index_1 = 0;
+	for (int i = 1; i < size; i = i + 1) {
+		if (*(arr + i) > *(arr + index_1)) {
+			index_1 = i;
+		}
+	}
+
+	int index_2 = 0;
+	if (index_1 == 0) {
+		index_2 = 1;
+	}
+
+	for (int i = 0; i < size; i = i + 1) {
+		if ((*(arr + i) != *(arr + index_1)) && (*(arr + i) > *(arr + index_2)) ){
+			index_2 = i;
+		}
+	}
+
+	return *(arr + index_1) * *(arr + index_2);*/
+
+	int index = 0;
+	for (int i = 1; i < size; i = i + 1) {
+		if (*(arr + i) > *(arr + index)) {
+			index = i;
+		}
+	}
+
+	int bigdog = *(arr + index);
+	*(arr + index) = 1;
+
+	index = 0;
+	for (int i = 1; i < size; i = i + 1) {
+		if (*(arr + i) > *(arr + index)) {
+			index = i;
+		}
+	}
+
+	return bigdog * *(arr + index);
 }
 
--- max_product_naive.c
+++ max_product_naive.c
@@ -1,5 +1,22 @@
 #include "max_product.h"
 
+int max(int x, int y) {
+	if (x >= y) {
+		return x;
+	}
+	else {
+		return y;
+	}
+}
+
 int max_pairwise_product_naive(int * arr, int size){
-	return 0;
+	int product = 0;
+
+	for (int i = 0; i < size - 1; i = i + 1) {
+		for (int j = i + 1; j < size; j = j + 1) {
+			product = max(product, *(arr + i) * *(arr + j));
+		}
+	}
+
+	return product;
 }
